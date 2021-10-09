4fabb21d04cb813e50a4f35d12a61dc2749e9f6a
k911672 <k911672@gmail.com>
2021-10-09 16:30:14 +0900

    laravel

---
+++ backend/app/Http/Controllers/ProductController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index() {
+
+
+        ddd(Product::where('category', "弁当"));
+        // $items[] = Product::where('category', "弁当")->random(1);
+        // $items[] = Product::where('category', "カフェ")->random(1);
+        // $items[] = Product::where('category', "サラダ")->random(1);
+
+        // return response()->json(['res' => 'OK', "items" => $items]);
+
+
+    }
+}
