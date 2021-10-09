80f2ddbc03e9032b1c2ec2cc7e965d30a5cdc2f0
k911672 <k911672@gmail.com>
2021-10-09 16:39:46 +0900

    コントローラーの修正

--- backend/app/Http/Controllers/ProductController.php
+++ backend/app/Http/Controllers/ProductController.php
@@ -8,15 +8,10 @@ use App\Models\Product;
 class ProductController extends Controller
 {
     public function index() {
+        $items[] = Product::where('category', "弁当")->random(1);
+        $items[] = Product::where('category', "カフェ")->random(1);
+        $items[] = Product::where('category', "サラダ")->random(1);
 
-
-        ddd(Product::where('category', "弁当"));
-        // $items[] = Product::where('category', "弁当")->random(1);
-        // $items[] = Product::where('category', "カフェ")->random(1);
-        // $items[] = Product::where('category', "サラダ")->random(1);
-
-        // return response()->json(['res' => 'OK', "items" => $items]);
-
-
+        return response()->json(['res' => 'OK', "items" => $items]);
     }
 }
